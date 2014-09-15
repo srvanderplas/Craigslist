1299e86662a37d87e14f3aeaf1999dfebdd07edd
Susan VanderPlas <srvanderplas@gmail.com>
2014-09-15 16:52:19 -0500

    push and pull

--- scrapeCLlinuxServer.R
+++ scrapeCLlinuxServer.R
@@ -11,5 +11,7 @@ for(i in 1:20){
   write.csv(temp, paste0("./data/", now(), ".csv"))
 }
 
+system("git pull")
 system("git add ./data/*")
 system("git commit -a -m 'New data files'")
+system("git push")

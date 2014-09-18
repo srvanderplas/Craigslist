f776ea4b481c1339d925c343450e1d9ddfbd93af
Susan VanderPlas <srvanderplas@gmail.com>
2014-09-17 20:12:14 -0500

    Testing tor proxies

---
+++ TestTorProxy.R
@@ -0,0 +1,8 @@
+for (i in 1:5)
+{
+  system("(echo authenticate '\"trusassy\"'; echo signal newnym; echo quit) | nc localhost 9051")
+  Sys.sleep(6)
+  ip <- scrape("http://ifconfig.me/ip")[[1]]  
+  print(xmlValue(getNodeSet(ip, "//*/p")[[1]]))
+  Sys.sleep(1)
+}  

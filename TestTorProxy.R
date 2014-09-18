27988805e59b93c820f889613cde9dda9ca34bb6
Susan VanderPlas <srvanderplas@gmail.com>
2014-09-17 20:14:23 -0500

    fix password

--- TestTorProxy.R
+++ TestTorProxy.R
@@ -1,6 +1,6 @@
 for (i in 1:5)
 {
-  system("(echo authenticate '\"trusassy\"'; echo signal newnym; echo quit) | nc localhost 9051")
+  system("(echo authenticate '\"password\"'; echo signal newnym; echo quit) | nc localhost 9051")
   Sys.sleep(6)
   ip <- scrape("http://ifconfig.me/ip")[[1]]  
   print(xmlValue(getNodeSet(ip, "//*/p")[[1]]))
